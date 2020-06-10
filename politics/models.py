255b3d29872dc23b60441f4d8bdabbbcfa2bf827
vasvenk <vas.venk@berkeley.edu>
2020-06-09 18:10:02 -0700

    Moved models to app folder

--- politics/models.py
+++ politics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Models):
+    """Class that specifies a political Candidate"""
+    id = models.CharField(max_length=128, primary_key=True)
+    name = models.CharField(max_length=128)
+
+
+class Stance(models.Models):
+    """Links political candidates to all their stances on issues"""
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    issue = models.CharField(max_length=128)
+    stance = models.CharField(max_length=128)
+
+
+class Company(models.Models):
+    """Company and its donations to political candidates"""
+    name = models.CharField(max_legnth=128)
+
+
+class Donation(models.Models):
+    """Weak entity denoting donations from a specific company to a political candidate"""
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    amount = models.BigIntegerField()
