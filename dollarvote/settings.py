80e6a4d18b01b651ba627b88148263fcb65368d1
vasvenk <vas.venk@berkeley.edu>
2020-06-12 15:28:45 -0700

    What is going on

--- dollarvote/settings.py
+++ dollarvote/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'chromiumextension.apps.ChromiumextensionConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
