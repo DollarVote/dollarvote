1208523b63fa4d7e41cb2a1b578e5893ca3c22de
vasvenk <vas.venk@berkeley.edu>
2020-06-14 15:07:12 -0700

    Changed to class based views

--- chromiumextension/views.py
+++ chromiumextension/views.py
@@ -8,23 +8,11 @@ from chromiumextension.src.impact import calculate_impact
 # Create your views here.
 
 
-def get(request):
-    company = str(request.body)
-    print("Received company: " + company)
-    response_data = calculate_impact(company)
-    response = JsonResponse(response_data)
-    return response
-
-
 class ChromiumExtension(View):
-    pass
 
-
-# def get_company(request):
-#     """Returns a company's positions on listed issues to the Chrome Extension"""
-#     data = str(request.body)
-#     print("Received company: " + data)
-#     print(request.body)
-#     response_data = {"blm": 0.0, "climate": 0, "healthcare": 0.0}
-#     response = JsonResponse(response_data)
-#     return response
+    def get(self, request):
+        company = str(request.body)
+        print("Received company: " + company)
+        response_data = calculate_impact(company)
+        response = JsonResponse(response_data)
+        return response
