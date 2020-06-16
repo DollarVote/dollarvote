77f81c8398843c40fdd420fa632e5c7b0d0c1b60
vasvenk <vas.venk@berkeley.edu>
2020-06-16 11:52:56 -0700

    Added joelene request changes

--- chromiumextension/views.py
+++ chromiumextension/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import json
 from django.http import JsonResponse
 from django.views import View
+from django.core import serializers
 from chromiumextension.src.impact import calculate_impact
-
-# Create your views here.
 
 
 class ChromiumExtension(View):
 
     def get(self, request):
-        company = str(request.body)
-        print("Received company: " + company)
+        company = request.GET['company']
         response_data = calculate_impact(company)
-        response = JsonResponse(response_data)
+        print(response_data)
+        print(response_data.company)
+        print(response_data.issue)
+        print(response_data.blm)
+        print(response_data.climate)
+        print(response_data.healthcare)
+        serialized_response_data = serializers.serialize('json', [ response_data, ])
+        response = JsonResponse(serialized_response_data, safe=False)
         return response
