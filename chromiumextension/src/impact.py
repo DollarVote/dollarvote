2006c31409e6a67c644f16154c9c006f61a41ff1
vasvenk <vas.venk@berkeley.edu>
2020-06-16 22:14:44 -0700

    Restructured code to a class based view

--- chromiumextension/src/impact.py
+++ chromiumextension/src/impact.py
@@ -1,88 +1,98 @@
 from chromiumextension.models import Donation, Stance, Impact
 from chromiumextension.constants import Issues, ImpactChannel
 
-channel_functions = {"donation": lambda x, y: donation_impact(x, y),
-                     "pledge": lambda x, y: pledge_impact(x, y)}
 stance_map = {"Yes": 1.0, "No": -1.0}
 
 
-def company_impact(company):
-    """Creates an aggregated score of a company's impact toward different issues.
-    TODO:
-        1.Only lookup issues specific to the user.
-        2.Add channels only specific to the user.
-        3.Customize channel weights to the user."""
-    user_issues = Issues.all_issues
-    user_channels = ImpactChannel.all_channels
-    channel_weights = {"donation": 0.7, "pledge": 0.3}
+class ImpactFactor:
 
-    complete_impact = dict([(issue, 0.0) for issue in user_issues])
-    for channel in user_channels:
-        channel_func = channel_functions[channel]
-        channel_impact = channel_func(company, user_issues)
-        for issue in user_issues:
-            complete_impact[issue] += channel_weights[channel] * channel_impact[issue]
+    def __init__(self, company):
+        self.company = company
+        self.issues = Issues.all_issues
 
-    return complete_impact
+    def company_impact(self):
+        """Creates an aggregated score of a company's impact toward different issues.
+        TODO:
+            1.Only lookup issues specific to the user.
+            2.Add channels only specific to the user.
+            3.Customize channel weights to the user."""
+        user_issues = Issues.all_issues
+        user_channels = ImpactChannel.all_channels
+        channel_weights = {"donation": 0.7, "pledge": 0.3}
 
+        complete_impact = dict([(issue, 0.0) for issue in user_issues])
+        for channel in user_channels:
+            channel_impact = self.channel_impact(channel)
+            for issue in user_issues:
+                complete_impact[issue] += channel_weights[channel] * channel_impact[issue]
 
-def pledge_impact(company, issues):
-    pledge = {}
-    remaining_issues = set(issues)
+        return complete_impact
 
-    relevant_impact = Impact.objects.filter(company=company, channel=ImpactChannel.pledge, issue__in=issues)
-    for impact in relevant_impact:
-        pledge[impact.issue] = impact.score
-        remaining_issues.discard(impact.issue)
+    def channel_impact(self, channel):
+        if channel == ImpactChannel.pledge:
+            return self.pledge_impact()
+        elif channel == ImpactChannel.donation:
+            return self.donation_impact()
 
-    for issue in remaining_issues:
-        pledge[issue] = 0.0
+    def pledge_impact(self):
+        pledge = {}
+        remaining_issues = set(self.issues)
 
-    return pledge
+        relevant_impact = Impact.objects.filter(company=self.company,
+                                                channel=ImpactChannel.pledge,
+                                                issue__in=self.issues)
+        for impact in relevant_impact:
+            pledge[impact.issue] = impact.score
+            remaining_issues.discard(impact.issue)
 
+        for issue in remaining_issues:
+            pledge[issue] = 0.0
 
-def donation_impact(company, issues):
-    "Calculates the impact that company donations have on the issue in question."
-    found_impact = {}
-    remaining_issues = set(issues)
+        return pledge
 
-    relevant_impact = Impact.objects.filter(company=company, channel=ImpactChannel.donation, issue__in=issues)
-    for impact in relevant_impact:
-        found_impact[impact.issue] = impact.score
-        remaining_issues.discard(impact.issue)
+    def donation_impact(self):
+        """Calculates the impact that company donations have on the issue in question."""
+        found_impact = {}
+        remaining_issues = set(self.issues)
 
-    remaining_impact = {}
-    if remaining_issues:
-        remaining_impact = calculate_donation(company, remaining_issues)
+        relevant_impact = Impact.objects.filter(company=self.company,
+                                                channel=ImpactChannel.donation,
+                                                issue__in=self.issues)
+        for impact in relevant_impact:
+            found_impact[impact.issue] = impact.score
+            remaining_issues.discard(impact.issue)
 
-    donation = {}
-    donation.update(found_impact)
-    donation.update(remaining_impact)
+        remaining_impact = {}
+        if remaining_issues:
+            remaining_impact = self.calculate_donation(remaining_issues)
 
-    return donation
+        donation = {}
+        donation.update(found_impact)
+        donation.update(remaining_impact)
 
+        return donation
 
-def calculate_donation(company, issues):
-    """TODO: This function might need to be heavily modified"""
-    issue_weights = {}
-    donation_total = {}
-    for issue in issues:
-        issue_weights[issue] = 0
-        donation_total[issue] = 0
+    def calculate_donation(self, issues):
+        """TODO: This function might need to be heavily modified"""
+        issue_weights = {}
+        donation_total = {}
+        for issue in issues:
+            issue_weights[issue] = 0
+            donation_total[issue] = 0
 
-    all_donations = Donation.objects.filter(company__name=company)
-    for donation in all_donations:
-        candidate = donation.candidate
-        stances = Stance.objects.filter(candidate__id=candidate.id, issue__in=issues)
-        for stance in stances:
-            issue_weights[stance.issue] += donation.amount * stance_map[stance.stance]
-            donation_total[stance.issue] += donation.amount
+        all_donations = Donation.objects.filter(company__name=self.company)
+        for donation in all_donations:
+            candidate = donation.candidate
+            stances = Stance.objects.filter(candidate__id=candidate.id, issue__in=issues)
+            for stance in stances:
+                issue_weights[stance.issue] += donation.amount * stance_map[stance.stance]
+                donation_total[stance.issue] += donation.amount
 
-    issues_impact = {}
-    for issue in issues:
-        score = issue_weights[issue] / donation_total[issue]
-        donation = Impact(company=company, channel=ImpactChannel.donation,issue=issue, score=score)
-        donation.save()
-        issues_impact[issue] = score
+        issues_impact = {}
+        for issue in issues:
+            score = issue_weights[issue] / donation_total[issue]
+            donation = Impact(company=self.company, channel=ImpactChannel.donation,issue=issue, score=score)
+            donation.save()
+            issues_impact[issue] = score
 
-    return issues_impact
+        return issues_impact
--- chromiumextension/views.py
+++ chromiumextension/views.py
@@ -1,20 +1,15 @@
 from django.http import JsonResponse
 from django.views import View
 from django.core import serializers
-from chromiumextension.src.impact import donation_impact
+from chromiumextension.src.impact import ImpactFactor
 
 
 class ChromiumExtension(View):
 
     def get(self, request):
         company = request.GET['company']
-        response_data = donation_impact(company)
-        print(response_data)
-        print(response_data.company)
-        print(response_data.issue)
-        print(response_data.blm)
-        print(response_data.climate)
-        print(response_data.healthcare)
-        serialized_response_data = serializers.serialize('json', [ response_data, ])
+        impact_factor = ImpactFactor(company)
+        issue_impact = impact_factor.company_impact()
+        serialized_response_data = serializers.serialize('json', [issue_impact, ])
         response = JsonResponse(serialized_response_data, safe=False)
         return response
